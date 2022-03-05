7f03b88df63b6016b39f57c06c374323ee183f4c
PROSOLVER <94039049+PranavShrivastava25@users.noreply.github.com>
2022-03-05 23:11:33 +0530

    Create SnakeGame.java

---
+++ Java/snakeGame/SnakeGame.java
@@ -0,0 +1,8 @@
+// here we are just calling the gameframe constructor
+package com.pranav;
+
+public class SnakeGame {
+    public static void main(String[] args) {
+        GameFrame frame=new GameFrame();
+    }
+}
